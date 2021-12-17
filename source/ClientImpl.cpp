fffa04669314993a0c856db252c138ce993a1de9
Daniel Lundborg <daniel.lundborg@sandvik.com>
2021-12-17 08:28:28 +0000

    ClientImpl: Use cpp-httplib instead of cpr
    Signed-off-by: Daniel Lundborg <daniel.lundborg@sandvik.com>

--- source/ClientImpl.cpp
+++ source/ClientImpl.cpp
@@ -1,6 +1,6 @@
 #include "ClientImpl.h"
 
-#include <cpr/cpr.h>
+#include <httplib/httplib.h>
 
 #include <regex>
 
@@ -8,22 +8,45 @@
 
 using namespace funny;
 
+// getHostAndEndpoint will get the host and path part of an url
+bool getHostAndEndpoint(const std::string &url, std::string &host, std::string &path)
+{
+    std::size_t pos = 0;
+    for (int i = 0; i < 3; ++i)
+    {
+        pos = url.find('/', pos + 1);
+        if (pos == std::string::npos)
+        {
+            return false;
+        }
+    }
+    host = url.substr(0, pos);
+    path = url.substr(pos);
+    return true;
+}
+
 const std::string &ClientImpl::request(const std::string &url,
                                        const std::map<std::string, std::string> &headers)
 {
     spdlog::info("Sending request to {}.", url);
     _response = "";
-    auto h = cpr::Header();
-    for (auto &&hh : headers)
+    auto h = httplib::Headers(headers.begin(), headers.end());
+
+    std::string host, path;
+    if (!getHostAndEndpoint(url, host, path))
     {
-        h[hh.first] = hh.second;
-    }
-    cpr::Response r = cpr::Get(cpr::Url{url}, h);
-    if (r.status_code != 200)
-    {
-        spdlog::error("Received {} from after requesting to '{}'.", r.status_code, url);
+        spdlog::error("Failed parsing the url '{}'.", url);
         return _response;
     }
-    _response = r.text;
+
+    httplib::Client c(host);
+    httplib::Result r = c.Get(path.c_str(), h);
+    if (r.error() != httplib::Error::Success)
+    {
+        spdlog::error("Received {} with error {} after requesting to '{}'.", r.value().status,
+                      httplib::to_string(r.error()), url);
+        return _response;
+    }
+    _response = r.value().body;
     return _response;
 }
